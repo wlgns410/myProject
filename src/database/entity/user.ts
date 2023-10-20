6697c65096cc0be30eff38c7e3c003cfedb80cb0
jihoon-LT <jihoon.lee@lighthouse.love>
2023-10-20 18:31:24 +0900

    🚧 비밀번호 삽입 코드 작성, 레포지토리 작성중

--- src/database/entity/user.ts
+++ src/database/entity/user.ts
@@ -1,4 +1,5 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, BeforeInsert } from 'typeorm';
+import bcrypt from 'bcrypt';
 import TimeDefaultEntity from './TimeDefaultEntity';
 
 @Entity()
@@ -6,15 +7,27 @@ export class User extends TimeDefaultEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ nullable: true, length: 150 })
-  email: string | null;
+  @Column({ nullable: false, length: 150 })
+  email: string;
+
+  @Column({ nullable: false })
+  password: string;
 
   @Column({ nullable: true })
-  password: string | null;
+  name: string | null;
 
-  @Column()
-  name: string;
+  @Column({ nullable: false, length: 11 })
+  phone: string;
 
-  @Column({ nullable: true, length: 11 })
-  phone: string | null;
+  @BeforeInsert()
+  async savePasswordWithEncrypt() {
+    if (this.password) {
+      this.password = await bcrypt.hash(this.password, 5);
+    }
+  }
+
+  async comparePassword(password: string) {
+    const result = await bcrypt.compare(password, this.password);
+    return result;
+  }
 }
--- src/database/repository/userRepository.ts
+++ src/database/repository/userRepository.ts
@@ -1,7 +1,18 @@
+import { Entity, PrimaryGeneratedColumn, Column, BeforeInsert } from 'typeorm';
+import bcrypt from 'bcrypt';
 import { Repository } from 'typeorm';
 import { injectable } from 'inversify';
 import { User } from '../entity';
 
 @injectable()
 export class UserRepository extends Repository<User> {
+    async isEmailExists(email: string): Promise<boolean> {
+        const users = await this.find({
+          where: { email },
+        });
+        if (users.length > 0) {
+          return true;
+        }
+        return false;
+    }
 }
