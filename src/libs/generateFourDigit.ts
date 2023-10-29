d8c6f336556111bdfbbd80861499bb566952dd2f
jihoon-LT <jihoon.lee@lighthouse.love>
2023-10-29 10:56:03 +0900

    ✏️ 랜덤인증번호 type 추가

--- src/libs/generateFourDigit.ts
+++ src/libs/generateFourDigit.ts
@@ -1,8 +1,8 @@
-const generateFourDigitRandom = () => {
+const generateFourDigitRandom = (): number => {
     const min = 1000;
     const max = 9999;
     const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
     return randomNumber;
-  }
-  
+};
+
 export default generateFourDigitRandom;
