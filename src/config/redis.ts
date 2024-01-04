16ffe4a5e138efe307c6fa71d82c807ca58c97ae
wlgns410 <wlgns410@gmail.com>
2024-01-04 18:00:08 +0900

    🎨 레디스 환경설정 코드 추가

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,25 +1,57 @@
 import * as redis from 'redis';
+import path from 'path';
+import dotenv from 'dotenv';
 
-// const redisCli = redis.createClient({password: 'testuser', socket: {host: "redis-server", port: 6379}})
+if (process.env.NODE_ENV === 'production') {
+  dotenv.config({ path: path.join('.env.production') });
+} else if (process.env.NODE_ENV === 'local') {
+  dotenv.config({ path: path.join('.env.local') });
+}
+
+const NODE_ENV = process.env.NODE_ENV || 'local';
+const { REDIS_USER_NAME, REDIS_USER_PASSWORD, REDIS_USER_HOST, REDIS_USER_PORT } = process.env;
+const {
+  REDIS_PRODUCTION_USER_NAME,
+  REDIS_PRODUCTION_USER_PASSWORD,
+  REDIS_PRODUCTION_USER_HOST,
+  REDIS_PRODUCTION_USER_PORT,
+} = process.env;
+
+const config = {
+  local: {
+    host: REDIS_USER_HOST,
+    port: Number(REDIS_USER_PORT),
+    username: REDIS_USER_NAME,
+    password: REDIS_USER_PASSWORD,
+  },
+  production: {
+    host: REDIS_PRODUCTION_USER_HOST,
+    port: Number(REDIS_PRODUCTION_USER_PORT),
+    username: REDIS_PRODUCTION_USER_NAME,
+    password: REDIS_PRODUCTION_USER_PASSWORD,
+  },
+};
 
 // * Redis 연결
+const selectedConfig = config[NODE_ENV];
+
 const redisClient = redis.createClient({
   legacyMode: true,
-  username: 'default',
-  password: 'testuser',
+  username: selectedConfig.username,
+  password: selectedConfig.password,
   socket: {
-    host: 'redis-server',
-    port: 6379,
+    host: selectedConfig.host,
+    port: selectedConfig.port,
   },
-}); // legacy 모드 반드시 설정 !!
+});
 redisClient.on('connect', () => {
   console.info('Redis connected!');
 });
 redisClient.on('error', (err) => {
   console.error('Redis Client Error', err);
-  redisCli.quit(); // 또는 redisCli.end();
+  redisCli.quit();
 });
-redisClient.connect().then(); // redis v4 연결 (비동기)
-const redisCli = redisClient.v4; // 기본 redisClient 객체는 콜백기반인데 v4버젼은 프로미스 기반이라 사용
+redisClient.connect().then();
+const redisCli = redisClient.v4;
 
 export default redisCli;
